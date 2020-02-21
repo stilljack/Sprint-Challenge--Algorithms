bdc4d8faf73aca220dad801685578b549d2037c5
Jack Seymour <stilljack@gmail.com>
2020-02-21 09:18:11 -0800

    pretty much there on counth, need to fix the handling of the backwards case the function itself is ugly and could be simplied of course, will hopefully get time for that after short answer Qs and revisiting the robot sorter

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -2,9 +2,39 @@
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
+
+can't contain loops,
+says nothing about defining global variables
+
 '''
-def count_th(word):
-    
-    # TBC
-    
-    pass
+
+
+def count_th(word:str,result=0):
+    print(word)
+    if len(word) == 0:
+        print(f"final final count = {result}")
+        finalfinal=result
+        print(f"result = {result} finalfinal ={finalfinal}")
+        return int(f"{finalfinal}")
+    if len(word) < 3:
+        chunk = word[0:2]
+        print(f"chunk = {chunk}")
+        if chunk=="th" or chunk =="ht":
+            result+=1
+        print(f"final count = {result}")
+        return count_th(word[2:len(word)],result)
+    else:
+        chunk = word[0:2]
+        chunk2 = word[1:3]
+        print(f"chunk = {chunk}")
+        if chunk=="th" or chunk =="ht":
+            result+=1
+        elif chunk2=="th" or chunk =="ht":
+            result+=1
+            return count_th(word[3:len(word)],result)
+        return count_th(word[2:len(word)],result)
+
+
+
+
+
